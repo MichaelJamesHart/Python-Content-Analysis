003af0897549604f877eafe2e5b372db910a48bd
Michael J. Hart <m.james.hart@gmail.com>
2021-11-06 10:16:07 +0900

    Update text_analysis.py

--- text_analysis.py
+++ text_analysis.py
@@ -1,18 +1,20 @@
+#Michael Hart
+
 """
-We will open a text document and get information about it
+This program opens a text document and get information about it.
 """
 
 def remove_characters(full_str, remove_chars):
-    'returns version of string full_str without remove_chars characters'
+    'Returns version of string full_str without remove_chars characters.'
 
-    # make remove_chars into a list
+    # Make remove_chars into a list.
     remove_char_list = []
     remove_char_list += remove_chars
 
-    # set up string to return that will eventually not have the remove_chars
+    # Set up string to return that will eventually not have the remove_chars.
     clean_str = full_str
 
-    # go through each of the characters in remove_char_list and remove them from clean_str
+    # Go through each of the characters in remove_char_list and remove them from clean_str.
     for no_char in remove_char_list:
         clean_str = clean_str.replace(no_char, '')
 
@@ -20,15 +22,14 @@ def remove_characters(full_str, remove_chars):
     
 
 def remove_stop_words(word_list, stop_words):
-    'returns a version of word_list without words in stop_words'
+    'Returns a version of word_list without words in stop_words.'
 
-    # make a copy of word_list so we don't modify the original
+    # Make a copy of word_list so we don't modify the original.
     clean_list = word_list.copy()
 
-    # go through each stop word
+    # Go through each stop word.
     for stop_word in stop_words:
-        # remove the stop word from list
-
+        # Remove the stop word from list.
         words_left_to_remove = True
         while words_left_to_remove:
             try:
@@ -36,24 +37,24 @@ def remove_stop_words(word_list, stop_words):
             except:
                 words_left_to_remove = False
 
-    # return the word_list without the stop_words
+    # Return the word_list without the stop_words.
     return clean_list
 
 
-# open file for reading
+# Open file for reading.
 text_file = open("text_files/usdeclar.txt")
 
-# read content from text file (content variable is a string)
+# Read content from text file (content variable is a string).
 content = text_file.read()
 
 #
-# Get number of characters and inform user
+# Get number of characters and inform user.
 #
 num_characters = len(content)
 print("This file has " + str(num_characters) + " characters")
 
 #
-# get the number of words
+# Get the number of words.
 #
 content_no_punctuation = remove_characters(content, '.,;!?:-\'"$0987654321()')
 content_no_punctuation = content_no_punctuation.lower()
@@ -62,7 +63,7 @@ num_words = len(word_list)
 print("This file has " + str(num_words) + " words")
 
 #
-# get word frequency
+# Get word frequency.
 #
 import collections
 
@@ -72,20 +73,20 @@ print("The 20 most common words are " + str(top_20_words))
 
 
 #
-# get rid of stop words
+# Get rid of stop words.
 #
 
-# get list of stop words
+# Get list of stop words.
 stop_words_file = open("text_files/stop_words.txt")
 stop_words_content = stop_words_file.read()
 stop_words = stop_words_content.split()
 
-# get the word counts without the stop words
+# Get the word counts without the stop words.
 clean_word_list = remove_stop_words(word_list, stop_words)
 word_counter = collections.Counter(clean_word_list)
 top_20_words = word_counter.most_common(20)
 print("The 20 most common words are " + str(top_20_words))
 
 
-# close file
+# Close the file.
 text_file.close()
